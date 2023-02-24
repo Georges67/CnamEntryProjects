cf2e8ce9f4d97c9ccbd6465531b59f2c65ab5969
Georges FRANCISCO <georges.francisco@al-enterprise.com>
2023-02-24 09:23:42 +0100

    Add TP6 Project

---
+++ TP6/app/src/main/java/com/tp/myapp1/MainActivity.java
@@ -0,0 +1,178 @@
+package com.tp.myapp1;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+
+public class MainActivity extends Activity {
+
+    private TextView m_serviceInfoView = null;
+    private SeekBar m_mySeekBar;
+    private ProgressBar m_myProgressBar;
+    private TextView m_myProgressTextView;
+    private ToggleButton m_startServiceButton= null;
+    private ToggleButton m_bindButton;
+    private ToggleButton m_startButton= null;
+    private boolean m_isBound;
+    private Messenger m_service;
+    final Messenger m_messenger = new Messenger(new IncomingHandler());
+    private Context m_context;
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MyTestService.MSG_SET_INT_VALUE:
+//                    m_serviceInfoView.setText("Int Message: " + msg.arg1);
+                    m_myProgressBar.setProgress(msg.arg1);
+                    break;
+                case MyTestService.MSG_SET_STRING_VALUE:
+                    String str1 = msg.getData().getString("str1");
+                    m_myProgressTextView.setText("Str Message: " + str1);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    private ServiceConnection m_connection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            m_service = new Messenger(service);
+
+            // TODO 4) Show message "Connected" in Activity Screen
+
+            sendMsgToService(MyTestService.MSG_REGISTER_CLIENT);
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been unexpectedly disconnected - process crashed.
+            m_service = null;
+            // TODO 5) Show message "Disconnected" in Activity Screen
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        m_context = getApplicationContext();
+
+        m_startServiceButton = findViewById(R.id.start_service);
+//        m_startButton = findViewById(R.id.start_button);
+//        m_bindButton = findViewById(R.id.bind);
+
+        m_myProgressBar = findViewById(R.id.myProgressBar);
+        m_serviceInfoView = findViewById(R.id.service_info);
+        m_myProgressTextView = findViewById(R.id.progressValueTextView);
+        m_mySeekBar = findViewById(R.id.mySeekBar);
+
+        m_mySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                m_myProgressBar.setProgress(i);
+                m_myProgressTextView.setText(String.format("Progress ; %d", i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void onClickStartService(View view) {
+        // TODO 2) Fill StartService ToggleButton behavior
+    }
+
+    // TODO 3)  add method onCLick for bind and Start ToggleButtons
+
+    public void onClickBind(View view) {
+        if( m_bindButton.isChecked() ) {
+            m_bindButton.setText(m_context.getString(R.string.unbind));
+            doBindService();
+            m_startButton.setEnabled(true);
+        }
+        else
+        {
+            m_bindButton.setText(m_context.getString(R.string.bind));
+            doUnbindService();
+            m_startButton.setEnabled(false);
+            m_startButton.setChecked(false);
+            m_serviceInfoView.setText("Disconnected.");
+        }
+    }
+
+    public void onClickStart(View view) {
+        if( m_startButton.isChecked() ) {
+            m_startButton.setText(m_context.getString(R.string.stop));
+            sendMsgToService(MyTestService.MSG_START_COUNTER);
+        } else {
+            m_startButton.setText(m_context.getString(R.string.start));
+            sendMsgToService(MyTestService.MSG_STOP_COUNTER);
+        }
+    }
+
+    private View.OnClickListener m_resetListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view)
+        {
+            sendMsgToService(MyTestService.MSG_RESET_COUNTER);
+        }
+    };
+
+    private void sendMsgToService(int msgId) {
+        try {
+            Message msg = Message.obtain(null, msgId);
+            msg.replyTo = m_messenger;
+            m_service.send(msg);
+        } catch (RemoteException e) {
+            // In this case the service has crashed before we could even do anything with it
+        }
+    }
+
+    void doBindService() {
+        bindService(new Intent(this, MyTestService.class), m_connection, Context.BIND_AUTO_CREATE);
+        m_isBound = true;
+        m_serviceInfoView.setText("Binding");
+    }
+
+    void doUnbindService() {
+        if (m_isBound) {
+            // If we have received the service, and hence registered with it, then now is the time to unregister.
+            if (m_service != null) {
+                try {
+                    Message msg = Message.obtain(null, MyTestService.MSG_UNREGISTER_CLIENT);
+                    msg.replyTo = m_messenger;
+                    m_service.send(msg);
+                } catch (RemoteException e) {
+                    // There is nothing special we need to do if the service has crashed.
+                }
+            }
+            // Detach our existing connection.
+            unbindService(m_connection);
+            m_isBound = false;
+            m_serviceInfoView.setText("Disconnected.");
+        }
+    }
+}
---
+++ TP6/app/src/main/java/com/tp/myapp1/MyTestService.java
@@ -0,0 +1,184 @@
+package com.tp.myapp1;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by georges on 14/12/15.
+ */
+public class MyTestService extends Service
+{
+    private final String LOG_TAG = "MyTestService";
+
+    int mNotificationId = 1;
+
+    private Timer m_timer = null;
+    private int m_counter = 0, incrementBy = 1;
+    private static boolean isRunning = false;
+
+    ArrayList<Messenger> m_clients = new ArrayList<>(); // Keeps track of all current registered clients.
+
+    static final int MSG_REGISTER_CLIENT = 1;
+    static final int MSG_UNREGISTER_CLIENT = 2;
+    static final int MSG_SET_INT_VALUE = 3;
+    static final int MSG_SET_STRING_VALUE = 4;
+    static final int MSG_RESET_COUNTER = 5;
+    static final int MSG_START_COUNTER = 6;
+    static final int MSG_STOP_COUNTER = 7;
+    final Messenger m_messenger = new Messenger(new IncomingHandler()); // Target we publish for clients to send messages to IncomingHandler.
+    private NotificationManager m_notifyMgr;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i("MyService", ">onBind");
+        return m_messenger.getBinder();
+    }
+
+    class IncomingHandler extends Handler { // Handler of incoming messages from clients.
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_REGISTER_CLIENT:
+                    m_clients.add(msg.replyTo);
+                    break;
+                case MSG_UNREGISTER_CLIENT:
+                    m_clients.remove(msg.replyTo);
+                    break;
+                case MSG_SET_INT_VALUE:
+                    incrementBy = msg.arg1;
+                    break;
+                case MSG_RESET_COUNTER:
+                    m_counter = 0;
+                    break;
+                case MSG_START_COUNTER:
+                    Log.i("MyService", "Starting Timer");
+                    // TODO 6) create Timer and start it here
+                    m_timer = new Timer();
+                    m_timer.scheduleAtFixedRate(new TimerTask() {
+                        public void run() {
+                            onTimerTick();
+                        }
+                    }, 0, 500L);
+
+                    break;
+                case MSG_STOP_COUNTER:
+                    Log.i("MyService", "Stopping Timer");
+                    if (m_timer != null)
+                    {
+                        m_timer.cancel();
+                        m_timer = null;
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    private void sendMessageToUI(int valueToSend)
+    {
+        for (int i= m_clients.size()-1; i>=0; i--)
+        {
+            try
+            {
+                // Send data as an Integer
+                m_clients.get(i).send(Message.obtain(null, MSG_SET_INT_VALUE, valueToSend, 0));
+
+                //Send data as a String
+                Bundle b = new Bundle();
+                b.putString("str1", String.valueOf(valueToSend));
+                Message msg = Message.obtain(null, MSG_SET_STRING_VALUE);
+                msg.setData(b);
+                m_clients.get(i).send(msg);
+            }
+            catch (RemoteException e) {
+                // The client is dead. Remove it from the list; we are going through the list from back to front so this is safe to do inside the loop.
+                m_clients.remove(i);
+            }
+        }
+    }
+
+    @Override
+    public void onCreate()
+    {
+        Log.i("MyService", ">onCreate");
+
+        super.onCreate();
+        Log.i("MyService", "Service Started.");
+        showNotification();
+
+        isRunning = true;
+    }
+
+    private void showNotification()
+    {
+        Notification.Builder mBuilder =
+                new Notification.Builder(this)
+                        .setSmallIcon(R.drawable.moto)
+                        .setContentTitle("My Service Notification")
+                        .setContentText("The Service is Started");
+
+        // Gets an instance of the NotificationManager service
+        m_notifyMgr =(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Builds the notification and issues it.
+        m_notifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        Log.i("MyService", "Received start id " + startId + ": " + intent);
+        return START_STICKY; // run until explicitly stopped.
+    }
+
+    public static boolean isRunning()
+    {
+        return isRunning;
+    }
+
+
+    private void onTimerTick()
+    {
+        Log.i("TimerTick", "Timer doing work." + m_counter);
+        try
+        {
+            m_counter += incrementBy;
+            sendMessageToUI(m_counter);
+        }
+        catch (Throwable t)
+        {
+            //you should always ultimately catch all exceptions in m_timer tasks.
+            Log.e("TimerTick", "Timer Tick Failed.", t);
+        }
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        Log.i("MyService", ">onDestroy");
+
+        super.onDestroy();
+        if (m_timer != null)
+        {
+            m_timer.cancel();
+        }
+        m_counter = 0;
+        m_notifyMgr.cancel(mNotificationId); // Cancel the persistent notification.
+        Log.i("MyService", "Service Stopped.");
+        isRunning = false;
+    }
+}
